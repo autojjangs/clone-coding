6fc550ec8554208667a929710ce636cc66621353
장기준 <autojjangs@naver.com>
2025-01-03 22:04:24 +0900

    'first_commit'

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(width: 50, height: 50, color: Colors.blue),
+    );
+  }
+}
